747a002a782cde3c733f90e5596a5515644ea4ae
Edvinas01 <edvinas108@gmail.com>
2020-01-16 13:48:36 +0100

    Clear CF cache on deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,14 @@ if [[ "${CIRCLE_BRANCH}" == "${GH_SRC_BRANCH}" ]]; then
   git add -A
   git commit -m "Automated deployment: ${CIRCLE_SHA1}" --allow-empty
   git push origin "${GH_DST_BRANCH}"
+
+  # Purge cache.
+  curl -X POST \
+    "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cache" \
+    -H "Authorization: Bearer ${CF_TOKEN}" \
+    -H "Content-Type: application/json" \
+    -d '{"purge_everything":true}'
+
 else
   echo "Skipping deployment from ${GH_SRC_BRANCH}"
 fi
